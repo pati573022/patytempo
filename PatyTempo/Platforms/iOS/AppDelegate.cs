ec10d7b8e6d2151e91ca87dfee7ca9a81d21169b
patylinda <patiangelita15@gmail.com>
2024-04-16 10:44:46 -0300

    paty 16/04

---
+++ PatyTempo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PatyTempo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
