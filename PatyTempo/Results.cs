a31dd7bc3cff83470e6b25a76c435c6b7c180de8
patylinda <patiangelita15@gmail.com>
2024-04-16 11:52:21 -0300

    paty 16\04

---
+++ PatyTempo/Results.cs
@@ -0,0 +1,20 @@
+namespace PatyTempo;
+
+public class results
+{
+    public int temp {get ; set; }
+    public string description {get;set;}
+    public string city {get;set;}
+    public double rain {get;set;}
+    public int humidity {get;set;}
+    public string sunrise {get;set;}
+    public string sunset {get;set;}
+    public string wind_speedy {get;set;}
+    public int wind_direction{get;set;}
+    public string moon_phase {get;set;}
+    public string img_id {get;set;}
+    public string currently {get;set;}
+    public string condition_code {get;set;}
+
+
+}
