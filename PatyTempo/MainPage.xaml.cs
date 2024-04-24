d252dbe341588d0e0755b011e4dc01803f11fe3f
paty <patiangelita15@gmail.com>
2024-04-24 11:51:13 -0300

    paty 24/04

--- PatyTempo/MainPage.xaml.cs
+++ PatyTempo/MainPage.xaml.cs
@@ -2,24 +2,33 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
-		InitializeComponent();
-	
-    Resultados= new Results();
-	PreencherTela()
+
+	InitializeComponent();
+    Resultados= new results();
+	PreencherTela();
+
 	}
 
-{voidPreencherTela()
+	void PreencherTela()
+{
+     labelTemp.Text= Resultados.temp.Tostring();
+     labelSky.Text= Resultados.description;
+     labelCidade.Text= Resultados.city;
+     labelChuva.Text= Resultados.rain.Tostring();
+     labelHumidade.Text= Resultados.humidity.Tostring();
+     labelAmanhecer.text= Resultados.sunrise;
+     labelAnoitecer.Text= Resultados.sunset;
+     labelForcawind.Text= Resultados.wind_speedy.Tostring();
+     labelDirecaowind.Text= Resultados.wind_direction;
+     labelMoonFase.Text= Resultados.moon_phase;
+ }
+
+
+
+
+
+
+
 }
-
- labelTemp.Text= Resultados.temp.Tostring();
- labelSky.Text= Resultados.description;
- labelCidade.Text= Resultados.city;
- labelChuva.Text= Resultados.rain.Tostring();
- labelHumidade.Text= Resultados.humidity.Tostring();
-
-
-
